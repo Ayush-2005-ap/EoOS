6b93d9eaa924edc843d135355f79f4bb7c44e191
Ayush Pandey <apayush.20@gmail.com>
2026-06-09 12:15:08 +0530

    Update index text color hex code in header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
           <span className="font-plus-jakarta text-2xl font-extrabold text-secondary tracking-tight">
             EoOS
           </span>
-          <span className="font-plus-jakarta text-2xl font-extrabold text-[#ffcb05] tracking-tight">
+          <span className="font-plus-jakarta text-2xl font-extrabold text-[#ecae20] tracking-tight">
             Index
           </span>
         </Link>
