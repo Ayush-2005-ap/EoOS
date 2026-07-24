3ca0ae60cb538889dfb0c445bb6eb12da16eeeb5
Ayush Pandey <apayush.20@gmail.com>
2026-07-24 10:16:14 +0530

    refactor: comment out Chatbot component in root layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
             {children}
           </LaunchGuard>
         </Suspense>
-        <Chatbot />
+        {/* <Chatbot /> */}
       </body>
     </html>
   );
